995a89a0de032a6aee6e38ba06f62fa1d6d2b118
Vrishab Srivatsa <vsrivatsa25@gmail.com>
2020-08-29 15:28:26 +0530

    Added decision tree

--- ML/covid-dtree.py
+++ ML/covid-dtree.py
@@ -5,17 +5,17 @@ dataset = pd.read_csv('covidfinal1.csv')
 X = dataset.iloc[:, :-2]
 y = dataset.iloc[:, 14]
 from sklearn.preprocessing import StandardScaler
-xyz = 3
 
 sc = StandardScaler()
 X = sc.fit_transform(X)
-from sklearn.ensemble import RandomForestRegressor
 
-regressor = RandomForestRegressor(n_estimators=100, random_state=0)
+from sklearn import tree
+regressor = tree.DecisionTreeRegressor()
 regressor.fit(X, y)
 
 for i in range(1,100):
-    X_test = sc.transform([[1,0,i/100,1,0,1,0,1,0,1,0,1,0,1]])
+    X_test = sc.transform([[0,0,i/100,0,0,0,0,0,0,0,0,0,0,0]])
     y_pred = regressor.predict(X_test)
     plt.plot(i,y_pred,'ro')
+    print(i,y_pred)
 plt.show()
